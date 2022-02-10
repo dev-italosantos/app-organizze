56d1fa42bd2f8bde972965346778e8bf2137ee5b
Ítalo Santos <italotayllor@gmail.com>
2022-02-10 20:47:20 -0300

    feat: Adicionar atributos tela de cadastro

--- app/src/main/java/com/example/organizze/activity/CadastroActivity.java
+++ app/src/main/java/com/example/organizze/activity/CadastroActivity.java
@@ -1,16 +1,25 @@
 package com.example.organizze.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.organizze.R;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText editName, editEmail, editPassword;
+    private Button buttonCadast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        editName = findViewById(R.id.editName);
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+
     }
 }
