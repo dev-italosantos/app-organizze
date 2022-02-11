9f96b5375681ca411afe0456e822f0472e69357d
Ítalo Santos <italotayllor@gmail.com>
2022-02-10 21:44:34 -0300

    feat: Adicionar validação para que os campos não sejam nulos

--- app/src/main/java/com/example/organizze/activity/CadastroActivity.java
+++ app/src/main/java/com/example/organizze/activity/CadastroActivity.java
@@ -1,8 +1,10 @@
 package com.example.organizze.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +22,37 @@ public class CadastroActivity extends AppCompatActivity {
         editName = findViewById(R.id.editName);
         editEmail = findViewById(R.id.editEmail);
         editPassword = findViewById(R.id.editPassword);
+
+        buttonCadast = findViewById(R.id.buttonCadast);
+
+        buttonCadast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String campoName = editName.getText().toString();
+                String campoEmail = editEmail.getText().toString();
+                String campoPassword = editPassword.getText().toString();
+
+                if (!campoName.isEmpty()) {
+                    if (!campoEmail.isEmpty()) {
+                        if (!campoPassword.isEmpty()) {
+                            signUp();
+                        } else {
+                            Toast.makeText(CadastroActivity.this, "Preencha o password!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(CadastroActivity.this, "Preencha o email!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(CadastroActivity.this, "Preencha o nome!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void signUp() {
 
     }
 }
